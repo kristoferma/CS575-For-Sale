5538dc8ae29780993f12b84fce7f3044eca8d5bf
Kristófer Másson <kristoferma@me.com>
2017-10-27 11:14:44 -0400

    Setup firebase and firebase functions

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import firebase from 'firebase'
+
+var config = {
+  apiKey: 'AIzaSyDjC1dJFrFpBTiXlCGbMR4YY48RtfOTZV8',
+  authDomain: 'cs575-for-sale.firebaseapp.com',
+  databaseURL: 'https://cs575-for-sale.firebaseio.com',
+  projectId: 'cs575-for-sale',
+  storageBucket: 'cs575-for-sale.appspot.com',
+  messagingSenderId: '397055662302'
+}
+
+firebase.initializeApp(config)
+const database = firebase.database()
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { title: 'Welcome to React' }
+  }
+
+  componentWillMount() {
+    var starCountRef = database.ref('title/')
+    starCountRef.on('value', snapshot => {
+      this.setState({ title: snapshot.val() })
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{this.state.title}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
