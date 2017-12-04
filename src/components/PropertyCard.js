e2f3f2c79064b59a8581e6d084b1ad1cb49887db
Kapllani <levikapllani1@gmail.com>
2017-12-04 10:59:55 -0500

    clean up on property card css

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -6,11 +6,13 @@ import '../css/Common.css'
 const PropertyImage = ({ property }) => {
   if (typeof property !== 'number' || property < 1) property = 'err'
   return (
-    <div class={`no_margin_padding pixel_img card_base property_${property}`}>
+    <div class={`no_margin_padding pixel_img card_base`}>
       <img
         src={`${process.env.PUBLIC_URL}/img/${property}.png`}
         alt="property card"
       />
+      <p className = "top_left">{property}</p>
+      <p className = "bottom_right">{property}</p>
     </div>
   )
 }
